e455a595f924ab5f0f22487967ca12628d0eb5ff
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-08 13:56:27 +0500

    first commit

---
+++ dz9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dz9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n: ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            double summa = 0.0;
+            for (double i = n; i <= 2 * n; i++)
+            { 
+                summa += i * i;
+            }
+            Console.WriteLine($"Сумма будет {summa}");
+        }
+    }
+}
